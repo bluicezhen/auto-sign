56ddf9a342bc59a5e0ed2a997438f362932e5605
bluicezhen <bluice.zhen@gmail.com>
2024-11-04 20:32:30 +0800

    get

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var MAX_RETRY = 10
 
-function clickText(text, cn, isSimClick) {
+function getItem(text, cn) {
     for (var i =0; i < MAX_RETRY; i++) {
         sleep(1000)
         var item = className(cn)
@@ -15,33 +15,43 @@ function clickText(text, cn, isSimClick) {
                 console.error("Unsupported ClassName", cn)
                 exit(1)
         }
-        console.log('click text', text, item)
-
-        if (item !== null) {
-            if (isSimClick) {
-                var b = item.bounds()
-                click(b.centerX(), b.centerY());
-                break
-            }
-            item.click()
-            break
+        console.verbose('get item', cn, text, item)
+        if (!item !== null) {
+            return item
         }
     }
+    return null
+}
+
+function clickText(text, cn, isSimClick) {
+    var item = getItem(text, cn)
+
+    if (item === null) {
+        console.error('Can not found item', cn, text)
+        return
+    }
+
+    if (isSimClick) {
+        var b = item.bounds()
+        click(b.centerX(), b.centerY());
+        return
+    }
+
+    item.click()
 }
 
 
-var w = floaty.window(
-    <frame gravity="center">
-        <text id="text">悬浮文字</text>
-    </frame>
-);
+console.show(true);
 
 launch("com.jingdong.app.mall");
 clickText("我的", "android.view.View", false)
 clickText("京豆", "android.widget.TextView", true)
-clickText("去签到领京豆", "android.widget.TextView", true)
-clickText("签到领豆", "android.widget.TextView", true)
-back()
-clickText("全部领取", "android.widget.TextView", true)
+if (getItem("已签到", "android.widget.TextView") === null) {
+    clickText("去签到领京豆", "android.widget.TextView", true)
+    clickText("签到领豆", "android.widget.TextView", true)
+    back()
+} else {
+    console.log('已领取')
+}
 
-w.close();
+// clickText("全部领取", "android.widget.TextView", true)
