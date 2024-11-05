67f31ac4af5ded8627e8de2d053c257954563aab
bluicezhen <bluice.zhen@gmail.com>
2024-11-05 10:49:34 +0800

    fix

--- main.js
+++ main.js
@@ -11,12 +11,9 @@ function getItem(text, cn) {
             case "android.widget.TextView":
                 item = item.text(text).findOnce()
                 break
-            default:
-                console.error("Unsupported ClassName", cn)
-                exit(1)
         }
         console.verbose('get item', cn, text, item)
-        if (!item !== null) {
+        if (item !== null) {
             return item
         }
     }
@@ -41,17 +38,16 @@ function clickText(text, cn, isSimClick) {
 }
 
 
-console.show(true);
-
+// 京东 领京豆
 launch("com.jingdong.app.mall");
 clickText("我的", "android.view.View", false)
 clickText("京豆", "android.widget.TextView", true)
 if (getItem("已签到", "android.widget.TextView") === null) {
     clickText("去签到领京豆", "android.widget.TextView", true)
+    sleep(10000)
     clickText("签到领豆", "android.widget.TextView", true)
     back()
 } else {
     console.log('已领取')
 }
-
-// clickText("全部领取", "android.widget.TextView", true)
+clickText("全部领取", "android.widget.TextView", true)
