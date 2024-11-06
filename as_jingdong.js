31fb488dfd28935c540578a5eea6b38d2024df68
bluicezhen <bluice.zhen@gmail.com>
2024-11-06 13:52:24 +0800

    jd

--- as_jingdong.js
+++ as_jingdong.js
@@ -1,7 +1,7 @@
 var MAX_RETRY = 10
 
 function getItem(text, cn) {
-    for (var i =0; i < MAX_RETRY; i++) {
+    for (var i = 0; i < MAX_RETRY; i++) {
         sleep(1000)
         var item = className(cn)
         switch (cn) {
@@ -37,7 +37,6 @@ function clickText(text, cn, isSimClick) {
     item.click()
 }
 
-
 // 京东 领京豆
 launch("com.jingdong.app.mall");
 clickText("我的", "android.view.View", false)
@@ -46,11 +45,13 @@ if (getItem("已签到", "android.widget.TextView") === null) {
     clickText("去签到领京豆", "android.widget.TextView", true)
     sleep(10000)
     clickText("签到领豆", "android.widget.TextView", true)
+    sleep(10000)
     back()
 } else {
     console.log('已领取')
 }
 clickText("全部领取", "android.widget.TextView", true)
+sleep(10000)
 // 京东 一键价保
 back()
 clickText("客户服务", "android.widget.TextView", true)
